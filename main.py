b1c3fa58f5ddfe4e2c71884f2d6eb473b53040f2
saeed s <saeedsamadyar@yahoo.com>
2022-08-13 21:46:50 +0430

    11:54 AM THU 11/08/2022

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path="/Users/saeed/Downloads/chromedriver")
-driver.get("https://google.com/")
+driver.get("https://google.com")
+search_field = driver.find_element('name', 'q')
 
 
 
