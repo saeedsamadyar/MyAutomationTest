1b0ea926b36584bd3f7b7ef0237c75515436942d
saeed s <saeedsamadyar@yahoo.com>
2022-08-13 21:57:51 +0430

    11:54 AM THU 11/08/2022

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="/Users/saeed/Downloads/chromedriver")
-driver.get("https://google.com")
-search_field = driver.find_element('name', 'q')
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+#driver = webdriver.Chrome(executable_path="/Users/saeed/Downloads/chromedriver")
+driver = webdriver.Chrome(executable_path = ChromeDriverManager().install())
+
+driver.get("http://google.com")
+sleep(2)
+driver.find_element('name', 'q').send_keys("Wikipedia")
+sleep(3)
+driver.quit()
+#search_field = driver.find_element('name', 'q')
 
 
 
