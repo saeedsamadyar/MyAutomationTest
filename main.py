fa655fd99d7dc6ee2e214cd47e5fe2ab8c36081c
saeed s <saeedsamadyar@yahoo.com>
2022-08-18 09:48:12 +0430

    09:47 AM THU 18/08/2022

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
-# driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+## driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())  # With WebDriver Manager
 
 driver.get("http://google.com")
